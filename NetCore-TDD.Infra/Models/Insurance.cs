59469e3f346817bd5d5ddf1f889bc5835a2090f8
JBravo <jbravo@asociart.com.ar>
2020-06-11 20:16:07 -0300

    add new entity Insurance

---
+++ NetCore-TDD.Infra/Models/Insurance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore_TDD.Infra.Models
+{
+    public class Insurance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IdProvince { get; set; }
+        public bool IsActive { get; set; }
+
+        public Insurance()
+        {
+
+        }
+
+        public Insurance(int Id, string Name, int IdProvince, bool IsActive)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.IdProvince = IdProvince;
+            this.IsActive = IsActive;
+        }
+
+    }
+}
---
+++ NetCore-TDD.Test/Test/PostInsuranceTests.cs
@@ -0,0 +1,47 @@
+﻿using NetCore_TDD.Infra.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NetCore_TDD.Test.Test
+{
+    public class PostInsuranceTests
+    {
+        #region Theory
+        #endregion
+
+        #region Fact
+        //[Fact]
+        //public void Fact_PostInsurance_NoClassNoRepository()
+        //{
+        //    // EXAMPLE
+        //    var insurer = new Insurance(1,"Santader", 1, true);
+
+        //    // REPOSITORY
+        //    ins.Insurance.Add(insurer);
+        //    ins.SaveChanges();
+
+        //    // ASSERT
+        //    Assert.Equal(1, insurer.Id);
+
+        //}
+
+        [Fact]
+        public void Fact_PostInsurance_NoRepository()
+        {
+            // EXAMPLE
+            var insurer = new Insurance(1,"Santader", 1, true);
+
+            // REPOSITORY
+            ins.Insurance.Add(insurer);
+            ins.SaveChanges();
+
+            // ASSERT
+            Assert.Equal(1, insurer.Id);
+        }
+
+#endregion
+    
+}
+}
